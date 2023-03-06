f067db9baa1266d81a594bc0db179194a8227685
Tran Quoc Khanh Nguyen <tqknguyen@myseneca.ca>
2023-03-06 09:47:45 -0500

    Remove Loading

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Button, Card, Form, Container, Row, Badge } from "react-bootstrap";
 import Progress from "../components/Progress";
 import History from "../components/History";
-import Loading from "../components/Loading";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -12,7 +11,6 @@ import "../App.css";
 
 const Order = () => {
   const trackingData = useSelector((state) => state.tracking.data);
-  const isLoading = useSelector((state) => state.tracking.status);
   const [showTrackingDetail, setShowTrackingDetail] = useState(false);
   const [deliveryDate, setDeliveryDate] = useState();
   const [trackingItem, setTrackingItem] = useState({});
@@ -92,113 +90,105 @@ const Order = () => {
   return (
     <>
       <Container fluid="md">
-        {isLoading === "idle" ? (
-          <Loading />
-        ) : (
-          <>
-            <Row className={showTrackingDetail ? "hide" : "display"}>
-              <h1>Your Tracking Number</h1>
-              <span>
-                {" "}
-                <strong>Test 1: </strong>63fef402ed1d41cc68bb9f9d
-              </span>
-              <span>
-                {" "}
-                <strong>Test 2: </strong>63ffdeacba7d84d61d734c63
-              </span>
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control
-                    {...register("trackingID", {
-                      required: "Tracking ID is required",
-                    })}
-                    type="text"
-                    size="sm"
-                    placeholder="Eg: 3456shfiekf"
-                  />
-                  {errors.trackingID && (
-                    <Form.Text className="text-danger">
-                      {errors.trackingID.message}
-                    </Form.Text>
-                  )}
-                </Form.Group>
-                <MotionButton
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                  variant="primary"
-                  type="submit"
-                  disabled={
-                    Object.keys(errors).length > 0 || !isDirty || !isValid
-                  }
-                >
-                  Track
-                </MotionButton>
-              </Form>
-            </Row>
-
-            <Card
-              className={showTrackingDetail ? "display" : "hide"}
-              style={{ width: "100%" }}
-              bg="dark"
-              text="light"
+        <Row className={showTrackingDetail ? "hide" : "display"}>
+          <h1>Your Tracking Number</h1>
+          <span>
+            {" "}
+            <strong>Test 1: </strong>63fef402ed1d41cc68bb9f9d
+          </span>
+          <span>
+            {" "}
+            <strong>Test 2: </strong>63ffdeacba7d84d61d734c63
+          </span>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                {...register("trackingID", {
+                  required: "Tracking ID is required",
+                })}
+                type="text"
+                size="sm"
+                placeholder="Eg: 3456shfiekf"
+              />
+              {errors.trackingID && (
+                <Form.Text className="text-danger">
+                  {errors.trackingID.message}
+                </Form.Text>
+              )}
+            </Form.Group>
+            <MotionButton
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              variant="primary"
+              type="submit"
+              disabled={Object.keys(errors).length > 0 || !isDirty || !isValid}
             >
-              <Card.Header>
-                <h5>
-                  <strong>Tracking ID: </strong>
-                  <span>{trackingItem._id}</span>
-                </h5>
-                <h5>
-                  <strong>Status: </strong>
-                  <Badge
-                    bg={
-                      trackingItem.trackingStatus === "Delivered"
-                        ? "success"
-                        : "warning"
-                    }
-                  >
-                    {trackingItem.trackingStatus}
-                  </Badge>
-                </h5>
-                {trackingItem?.trackingStatus === "In Progress" && (
-                  <h5>
-                    <strong>Expected Delivery Date: </strong>
-                    <span>{deliveryDate}</span>
-                  </h5>
-                )}
+              Track
+            </MotionButton>
+          </Form>
+        </Row>
 
-                <div className="container-custom">
-                  <MotionButton
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    variant="outline-primary"
-                    onClick={trackMore}
-                  >
-                    Track more
-                  </MotionButton>
-                  <MotionButton
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    variant="outline-secondary"
-                    onClick={directToContact}
-                  >
-                    Contact us
-                  </MotionButton>
-                </div>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title>Your Order Progress</Card.Title>
-                <Progress trackingStatus={trackingItem.trackingStatus} />
-                <br />
-                <History
-                  orderNumber={trackingItem.orderNumber}
-                  orderDate={trackingItem.orderDate}
-                  pickingDate={trackingItem.pickingDate}
-                  note={trackingItem.note}
-                />
-              </Card.Body>
-            </Card>
-          </>
-        )}
+        <Card
+          className={showTrackingDetail ? "display" : "hide"}
+          style={{ width: "100%" }}
+          bg="dark"
+          text="light"
+        >
+          <Card.Header>
+            <h5>
+              <strong>Tracking ID: </strong>
+              <span>{trackingItem._id}</span>
+            </h5>
+            <h5>
+              <strong>Status: </strong>
+              <Badge
+                bg={
+                  trackingItem.trackingStatus === "Delivered"
+                    ? "success"
+                    : "warning"
+                }
+              >
+                {trackingItem.trackingStatus}
+              </Badge>
+            </h5>
+            {trackingItem?.trackingStatus === "In Progress" && (
+              <h5>
+                <strong>Expected Delivery Date: </strong>
+                <span>{deliveryDate}</span>
+              </h5>
+            )}
+
+            <div className="container-custom">
+              <MotionButton
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                variant="outline-primary"
+                onClick={trackMore}
+              >
+                Track more
+              </MotionButton>
+              <MotionButton
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                variant="outline-secondary"
+                onClick={directToContact}
+              >
+                Contact us
+              </MotionButton>
+            </div>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>Your Order Progress</Card.Title>
+            <Progress trackingStatus={trackingItem.trackingStatus} />
+            <br />
+            <History
+              orderNumber={trackingItem.orderNumber}
+              orderDate={trackingItem.orderDate}
+              pickingDate={trackingItem.pickingDate}
+              note={trackingItem.note}
+            />
+          </Card.Body>
+        </Card>
       </Container>
     </>
   );
